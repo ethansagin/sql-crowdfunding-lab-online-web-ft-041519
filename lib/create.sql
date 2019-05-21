9071084be6ee4c5d74d3b68711ea07a8531d953f
Ethan Sagin <ethansagin@gmail.com>
2019-05-21 20:41:27 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,5 @@
-CREATE TABLE project(title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
+CREATE TABLE project (title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
 
+CREATE TABLE user (name TEXT, age INTEGER);
+
+CREATE TABLE pledge (amount INTEGER);
